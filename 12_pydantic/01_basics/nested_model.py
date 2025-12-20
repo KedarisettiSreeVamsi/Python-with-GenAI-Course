f99fa6154566efca50d3b1ba029d5426d07140a1
KedarisettiSreeVamsi <ksreevamsi2027@gmail.com>
2025-12-20 19:40:42 +0530

    updated nested model

--- 12_pydantic/01_basics/nested_model.py
+++ 12_pydantic/01_basics/nested_model.py
@@ -7,9 +7,23 @@ class Address(BaseModel):
     postal_code: str
 
 class User(BaseModel):
+    id: int
     name: str
     address: Address
 
 
 address = Address(street="123",city="Vizag",postal_code="530032")
-user = User(name="Vamsi",address=address)
+user = User(id=1,name="Vamsi",address=address)
+
+user_data = {
+    "id":1,
+    "name":'Hitesh',
+    'address':{
+        'street':'321 something',
+        'city':"Paris",
+        'postal_code':'20002'
+    }
+}
+
+user = User(**user_data)
+print(user.model_dump_json())
